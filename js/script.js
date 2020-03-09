c278f9f3e948a755563a6b263f0ef12d7de45d84
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-03-10 00:18:38 +0700

    finish the game

--- js/script.js
+++ js/script.js
@@ -1,9 +1,41 @@
 $(document).ready(function() {
   let TerKlik = null;
+  let klikSebelumnya = false;
+  let kartuCocok = 0;
+
+  const semuaNama = [
+    "ricky",
+    "janu",
+    "umar",
+    "reza",
+    "purno",
+    "fuad",
+    "gani",
+    "adit"
+  ];
+
+  const kartu = [...$(".kartu")];
+  for (let nama of semuaNama) {
+    const kartuA_index = parseInt(Math.random() * kartu.length);
+    const kartuA = kartu[kartuA_index];
+    kartu.splice(kartuA_index, 1);
+    kartuA.className += ` ${nama}`;
+    kartuA.setAttribute("data-nama", nama);
+
+    const kartuB_index = parseInt(Math.random() * kartu.length);
+    const kartuB = kartu[kartuB_index];
+    kartu.splice(kartuB_index, 1);
+    kartuB.className += ` ${nama}`;
+    kartuB.setAttribute("data-nama", nama);
+  }
 
   $(".kartu").click(function(e) {
     const target = e.currentTarget;
-    if (target === TerKlik || target.className.includes("done")) {
+    if (
+      klikSebelumnya ||
+      target === TerKlik ||
+      target.className.includes("done")
+    ) {
       return;
     }
     target.className = target.className.replace("color-hidden", "").trim();
@@ -15,19 +47,26 @@ $(document).ready(function() {
       //jika tidak ada kartu yg di klik ,tetap tampilkan gambar
     } else if (TerKlik) {
       //jika ada kartu yg diklik, cek apakah kartu selanjutnya sama atau tidak
+      klikSebelumnya = true;
       if (
-        TerKlik.getAttribute("data-nama") === target.getAttribute("data-nama")
+        TerKlik.getAttribute("data-nama") !== target.getAttribute("data-nama")
       ) {
-        console.log("sama");
-        TerKlik = null;
-      } else {
+        klikSebelumnya = true;
         setTimeout(() => {
           TerKlik.className =
             TerKlik.className.replace("done", "").trim() + " color-hidden";
           target.className =
             target.className.replace("done", "").trim() + " color-hidden";
           TerKlik = null;
-        }, 800);
+          klikSebelumnya = false;
+        }, 500);
+      } else {
+        kartuCocok++;
+        TerKlik = null;
+        klikSebelumnya = false;
+        if (kartuCocok == 8) {
+          alert("YEY kamu CROTTTT");
+        }
       }
     }
   });
