007765e3244b7bbe2adc203e59843f217cca28fd
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-03-09 21:27:35 +0700

    add cover and update file js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-  $(".kartu").click(function() {
-    $(".kartu").css("color", "pink");
+  let TerKlik = undefined;
+
+  $(".kartu").click(function(e) {
+    $(this).toggleClass("color-hidden");
   });
+
+  if (!TerKlik) {
+    //jika tidak ada kartu yg di klik ,tetap tampilkan gambar
+  } else if (TerKlik) {
+    //jika ada kartu yg diklik, cek apakah kartu selanjutnya sama atau tidak
+  }
 });
